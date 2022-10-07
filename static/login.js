a6fd806c95399150aa7f6920f24909b5ac774787
Emilio Mendoza <Ironislife39@gmail.com>
2022-10-07 10:40:45 -0400

    add hide password

---
+++ static/login.js
@@ -0,0 +1,9 @@
+function hidden(){
+    let button = document.getElementById("hidebutton")
+    if (button.type == password){
+        button.type = "text";
+    }
+    else {
+        temp.type == "password"
+    }
+}
